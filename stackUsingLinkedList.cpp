3c35c4d20a290fd51983639d9758bf46fd274870
rrassya19-bit <rrassys19@gmail.com>
2026-05-15 14:49:29 +0700

    default case untuk menangani pilihan tidak valid

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -108,6 +108,9 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
         }
     }
     return 0;
