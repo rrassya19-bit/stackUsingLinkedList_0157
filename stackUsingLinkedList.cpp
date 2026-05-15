54b25f756f59a61cd7dfdbb5d241cc0c33ae6ac3
rrassya19-bit <rrassys19@gmail.com>
2026-05-15 14:41:26 +0700

    fungsi isEmpty untuk cek stack kosong

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -6,7 +6,7 @@ class Node
 public:
     int data;
     Node *next;
-    Node(int value)
+    Node()
     {
         next = NULL;
     }
@@ -16,10 +16,18 @@ class stack
 {
 private:
     Node *top;
-    
 public:
     stack()
     {
         top = NULL;
     }
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
 };
+
+int main()
+{
+    return 0;
+}
